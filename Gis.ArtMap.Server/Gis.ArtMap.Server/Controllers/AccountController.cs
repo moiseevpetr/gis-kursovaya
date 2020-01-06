8f04b0fbc737a3ace3f4cfc8de6a1749560d9856
Konstantin Beder <bederkb@sibedge.com>
2020-01-06 16:22:59 +0700

    add authentication and authorization

---
+++ Gis.ArtMap.Server/Gis.ArtMap.Server/Controllers/AccountController.cs
@@ -0,0 +1,74 @@
+﻿namespace Gis.ArtMap.Server.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Linq;
+    using System.Security.Claims;
+    using Entities;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.IdentityModel.Tokens;
+
+    public class AccountController : Controller
+    {
+        private readonly DbSet<User> users;
+
+        public AccountController(ArtMapDbContext context)
+        {
+            this.users = context.User;
+        }
+
+        [HttpPost("/token")]
+        public IActionResult Token(string username, string password)
+        {
+            ClaimsIdentity identity = GetIdentity(username, password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
+
+            DateTime now = DateTime.UtcNow;
+
+            var jwt = new JwtSecurityToken(
+                AuthOptions.ISSUER,
+                AuthOptions.AUDIENCE,
+                notBefore: now,
+                claims: identity.Claims,
+                expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            string encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+            return Json(response);
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            User person = this.users.FirstOrDefault(x => x.Name == username && x.Password == password);
+
+            if (person == null)
+            {
+                return null;
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, person.Name),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, person.UserRole.ToString())
+            };
+
+            var claimsIdentity =
+                new ClaimsIdentity(claims,
+                    "Token",
+                    ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+            return claimsIdentity;
+        }
+    }
+}
